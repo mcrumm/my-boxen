2910ff2ff0867eedb8811628eab1d6b9b5fdf31f
Michael Crumm <mike@crumm.net>
2014-12-02 00:52:10 -0800

    Removing unused services, adding packages

--- modules/people/manifests/mcrumm.pp
+++ modules/people/manifests/mcrumm.pp
@@ -14,6 +14,22 @@ class people::mcrumm {
     require => File[$my]
   }
 
+  service { "dev.nginx":
+    ensure => "stopped"
+  }
+
+  service { "dev.dnsmasq":
+    ensure => "stopped"
+  }
+
+  package { "pstree":
+    ensure => present,
+  }
+
+  package { "watch":
+    ensure => present,
+  }
+
   exec { "tap-homebrew-dupes":
     command => "brew tap homebrew/dupes",
     creates => "${homebrew::config::tapsdir}/homebrew-dupes",
