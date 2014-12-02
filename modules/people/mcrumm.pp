3cc3fd3d5ef1ea8be8d1f93b28247a9e7de56788
Michael Crumm <mike@crumm.net>
2014-12-01 23:39:00 -0800

    Adding mcrumm manifest

---
+++ modules/people/mcrumm.pp
@@ -0,0 +1,17 @@
+class people::mcrumm {
+  include vim
+  include chrome
+
+  $home     = "/Users/${::boxen_user}"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
+
+  file { $my:
+    ensure  => directory
+  }
+
+  repository { $dotfiles:
+    source  => 'mcrumm/dotfiles',
+    require => File[$my]
+  }
+}
