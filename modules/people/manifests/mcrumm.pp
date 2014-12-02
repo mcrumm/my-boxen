c8ad8f54fad2260fdb585ca87542b73876f805bd
Michael Crumm <mike@crumm.net>
2014-12-02 00:46:52 -0800

    Adding php55 from brew

--- modules/people/manifests/mcrumm.pp
+++ modules/people/manifests/mcrumm.pp
@@ -13,4 +13,24 @@ class people::mcrumm {
     source  => 'mcrumm/dotfiles',
     require => File[$my]
   }
+
+  exec { "tap-homebrew-dupes":
+    command => "brew tap homebrew/dupes",
+    creates => "${homebrew::config::tapsdir}/homebrew-dupes",
+  }
+
+  exec { "josegonzalez/homebrew-php":
+    command => "brew tap josegonzalez/homebrew-php",
+    creates => "${homebrew::config::tapsdir}/josegonzalez-php",
+    require => Exec["tap-homebrew-dupes"],
+  }
+
+  package { "php55":
+    ensure => present,
+    require => [
+      Exec["josegonzalez/homebrew-php"],
+      Package["pstree"],
+      Package["watch"],
+    ],
+  }
 }
