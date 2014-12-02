c04edc950ead0261d2b8d326ca4b98697fb36f80
Michael Crumm <mike@crumm.net>
2014-12-02 13:10:38 -0800

    Adding osx modifications

--- modules/people/manifests/mcrumm.pp
+++ modules/people/manifests/mcrumm.pp
@@ -43,4 +43,27 @@ class people::mcrumm {
   package { "watch":
     ensure => present
   }
+
+  # OSX configuations
+  include osx::global::disable_key_press_and_hold
+  include osx::global::expand_save_dialog
+  include osx::global::expand_save_dialog
+  include osx::dock::autohide
+  include osx::finder::show_all_on_desktop
+  include osx::finder::unhide_library
+  include osx::finder::show_hidden_files
+  include osx::finder::enable_quicklook_text_selection
+  include osx::finder::show_all_filename_extensions
+  include osx::no_network_dsstores
+  include osx::global::key_repeat_delay
+  include osx::global::key_repeat_rate
+  class { 'osx::global::natural_mouse_scrolling':
+    enabled => false
+  }
+  class { 'osx::dock::hot_corners':
+    top_right => "Start Screen Saver",
+  }
+  class { 'osx::sound::interface_sound_effects':
+    enable => false
+  }
 }
