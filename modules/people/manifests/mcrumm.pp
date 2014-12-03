4780906b6e75fb80c4cc51a26d8af63f2ed7dd4e
Michael Crumm <mike@crumm.net>
2014-12-03 11:03:33 -0800

    Fix git-flow include

--- modules/people/manifests/mcrumm.pp
+++ modules/people/manifests/mcrumm.pp
@@ -4,7 +4,7 @@ class people::mcrumm {
   include bash::completion
   include chrome
   include dropbox
-  include git-flow
+  include 'git-flow'
   include iterm2::stable
   include mou
   include mou::themes
